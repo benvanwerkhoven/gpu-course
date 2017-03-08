597fc32a886a925df9bcb6f28188b90a2ca59305
Ben van Werkhoven <b.vanwerkhoven@esciencecenter.nl>
2017-03-08 15:59:41 +0100

    added pnpoly

---
+++ pnpoly/pnpoly.cu
@@ -0,0 +1,249 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda.h>
+
+#define VERTICES 600
+
+extern "C" {
+    __global__ void cn_pnpoly(int *bitmap, float2 *points, float2 *vertices, int n);
+    __global__ void cn_pnpoly_reference_kernel(int *bitmap, float2 *points, float2 *vertices, int n);
+}
+
+/*
+ * This file contains the implementation of a CUDA Kernel for the
+ * point-in-polygon problem using the crossing number algorithm
+ *
+ * Simplified for use in the NLeSC GPU Course
+ *
+ * The algorithm used here is adapted from: 
+ *     'Inclusion of a Point in a Polygon', Dan Sunday, 2001
+ *     (http://geomalgorithms.com/a03-_inclusion.html)
+ *
+ * Author: Ben van Werkhoven <b.vanwerkhoven@esciencecenter.nl>
+ */
+__global__ void cn_pnpoly(int *bitmap, float2 *points, float2 *vertices, int n) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (i < n) {
+        int c = 0;
+        float2 p = points[i];
+
+        int k = VERTICES-1;
+
+        for (int j=0; j<VERTICES; k = j++) {    // edge from vk to vj
+            float2 vj = vertices[j]; 
+            float2 vk = vertices[k]; 
+
+            float slope = (vk.x-vj.x) / (vk.y-vj.y);
+
+            if ( (  (vj.y>p.y) != (vk.y>p.y)) &&            //if p is between vj and vk vertically
+                    (p.x < slope * (p.y-vj.y) + vj.x) ) {   //if p.x crosses the line vk-vj when moved in positive x-direction
+                c = !c;
+            }
+        }
+
+        bitmap[i] = c; // 0 if even (out), and 1 if odd (in)
+    }
+
+}
+
+
+
+
+
+
+int main() {
+
+    cudaSetDeviceFlags(cudaDeviceMapHost);
+    cudaSetDevice(0);
+    cudaDeviceSynchronize();
+
+    cudaError_t err;
+    int num_points = (int)2e7;
+
+    float2 *h_vertices;
+    float2 *d_vertices;
+    float2 *h_points;
+    int *h_bitmap;
+    int *h_reference;
+
+    //Allocate pinned and aligned host memory and copy input data
+    err = cudaHostAlloc((void **)&h_vertices, VERTICES*sizeof(float2), cudaHostAllocMapped);
+    if (err != cudaSuccess) {
+        fprintf(stderr, "Error in cudaHostAlloc: %s\n", cudaGetErrorString(err));
+    }
+    err = cudaHostAlloc((void **)&h_points, num_points *sizeof(float2), cudaHostAllocMapped);
+    if (err != cudaSuccess) {
+        fprintf(stderr, "Error in cudaHostAlloc: %s\n", cudaGetErrorString(err));
+    }
+    err = cudaHostAlloc((void **)&h_bitmap, num_points *sizeof(int), cudaHostAllocMapped);
+    if (err != cudaSuccess) {
+        fprintf(stderr, "Error in cudaHostAlloc: %s\n", cudaGetErrorString(err));
+    }
+    err = cudaHostAlloc((void **)&h_reference, num_points *sizeof(int), cudaHostAllocMapped);
+    if (err != cudaSuccess) {
+        fprintf(stderr, "Error in cudaHostAlloc: %s\n", cudaGetErrorString(err));
+    }
+
+    // generate random input
+    for (int i=0; i< num_points; i++) {
+        h_points[i].x = 50.0 / (rand() % 1000);
+        h_points[i].y = 50.0 / (rand() % 1000);
+    }
+    // read vertices from disk
+    FILE *file = fopen("vertices.dat", "rb");
+    fread(h_vertices, sizeof(float), 2*VERTICES, file);
+
+    // allocate device memory for storing the vertices
+    err = cudaMalloc((void **)&d_vertices, VERTICES*sizeof(float2));
+    if (err != cudaSuccess) {
+        fprintf(stderr, "Error in cudaMalloc: %s\n", cudaGetErrorString( err ));
+    }
+
+    // transfer vertices to d_vertices
+    err = cudaMemcpy(d_vertices, h_vertices, VERTICES*sizeof(float2), cudaMemcpyHostToDevice);
+    if (err != cudaSuccess) {
+        fprintf(stderr, "Error in cudaMemcpyToSymbol: %s\n", cudaGetErrorString(err));
+    }
+
+    // create CUDA streams and events
+    cudaStream_t stream[1];
+    err = cudaStreamCreate(&stream[0]);
+    if (err != cudaSuccess) {
+        fprintf(stderr, "Error in cudaStreamCreate: %s\n", cudaGetErrorString(err));
+    }
+    cudaEvent_t start;
+    err = cudaEventCreate(&start);
+    if (err != cudaSuccess) {
+        fprintf(stderr, "Error in cudaEventCreate: %s\n", cudaGetErrorString(err));
+    }
+
+    cudaEvent_t stop;
+    err = cudaEventCreate(&stop);
+    if (err != cudaSuccess) {
+        fprintf(stderr, "Error in cudaEventCreate: %s\n", cudaGetErrorString(err));
+    }
+
+    cudaDeviceSynchronize();
+    err = cudaGetLastError();
+    if (err != cudaSuccess) {
+        fprintf(stderr, "Error after memory setup: %s\n", cudaGetErrorString(err));
+    }
+
+    //kernel parameters
+    dim3 threads(256, 1, 1);
+    dim3 grid((int)ceil(num_points / (float)threads.x), 1);
+
+    //run the kernel a few times to warmup the device
+    for (int i=0; i<5; i++) {
+        cn_pnpoly_reference_kernel<<<grid, threads, 0, stream[0]>>>(h_reference, h_points, d_vertices, num_points);
+    }
+    memset(h_bitmap, 0, num_points*sizeof(int));
+
+    //start measuring time
+    cudaDeviceSynchronize();
+    cudaEventRecord(start, stream[0]);
+
+    //call the kernel
+    cn_pnpoly<<<grid, threads, 0, stream[0]>>>(h_bitmap, h_points, d_vertices, num_points);
+
+    //stop time measurement
+    cudaEventRecord(stop, stream[0]);
+    cudaDeviceSynchronize();
+    float time = 0.0;
+    cudaEventElapsedTime(&time, start, stop);
+    printf("cn_pnpoly kernel took: %f (ms)\n", time);
+
+
+
+    //compute reference answer and measure time
+    cudaDeviceSynchronize();
+    cudaEventRecord(start, stream[0]);
+    cn_pnpoly_reference_kernel<<<grid, threads, 0, stream[0]>>>(h_reference, h_points, d_vertices, num_points);
+    cudaEventRecord(stop, stream[0]);
+    cudaDeviceSynchronize();
+    cudaEventElapsedTime(&time, start, stop);
+    printf("reference kernel took: %f (ms)\n", time);
+
+    //cleanup
+    cudaStreamDestroy(stream[0]);
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+    cudaFree(d_vertices);
+    cudaFreeHost(h_vertices);
+    cudaFreeHost(h_points);
+
+    //final check for errors
+    cudaDeviceSynchronize();
+    err = cudaGetLastError();
+    if (err != cudaSuccess) {
+        fprintf(stderr, "Error after CUDA kernel: %s\n", cudaGetErrorString(err));
+        exit(1);
+    } else {
+        int errors = 0;
+        int print = 0;
+        for (int i=0; i<num_points; i++) {
+            if (h_bitmap[i] != h_reference[i]) {
+                errors++;
+                if (print++ < 10) {
+                    fprintf(stderr, "error at %d, reference=%d, answer=%d\n", i, h_reference[i], h_bitmap[i]);
+                }
+            }
+        }
+        if (errors == 0) {
+            printf("ok!\n");
+        } else {
+            printf("there were %d errors\n", errors);
+        }
+    }
+
+    cudaFreeHost(h_bitmap);
+    cudaFreeHost(h_reference);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ * Reference kernel
+ *
+ * This kernel is kept for checking the output of the above kernel, DO NOT MODIFY THIS KERNEL
+ */
+__global__ void cn_pnpoly_reference_kernel(int *bitmap, float2 *points, float2 *vertices, int n) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < n) {
+        int c = 0;
+        float2 p = points[i];   // DO NOT MODIFY THIS KERNEL
+        int k = VERTICES-1;
+        for (int j=0; j<VERTICES; k = j++) {
+            float2 vj = vertices[j];    // DO NOT MODIFY THIS KERNEL
+            float2 vk = vertices[k]; 
+            float slope = (vk.x-vj.x) / (vk.y-vj.y);
+            if ( (  (vj.y>p.y) != (vk.y>p.y)) && (p.x < slope * (p.y-vj.y) + vj.x) ) {
+                c = !c;
+            }
+        }
+        bitmap[i] = c; // DO NOT MODIFY THIS KERNEL
+    }
+}
+
---
+++ pnpoly/pnpoly.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+import numpy
+
+import pycuda.driver as drv
+from pycuda.compiler import SourceModule
+
+def allocate(n, dtype=numpy.float32):
+    """ allocate context-portable device mapped host memory """
+    return drv.pagelocked_zeros(int(n), dtype, order='C', mem_flags=drv.host_alloc_flags.PORTABLE|drv.host_alloc_flags.DEVICEMAP)
+
+def run_pnpoly(context, cc):
+
+    #read kernel into string
+    with open('pnpoly.cu', 'r') as f:
+        kernel_string = f.read()
+
+    #compile the kernels
+    module = SourceModule(kernel_string, arch='compute_' + cc, code='sm_' + cc,
+                    cache_dir=False, no_extern_c=True)
+    pnpoly_kernel = module.get_function("cn_pnpoly")
+
+    #set the number of points and the number of vertices
+    size = numpy.int32(2e7)
+    vertices = 600
+
+    #allocate page-locked device-mapped host memory
+    points = allocate(2*size, numpy.float32)
+    bitmap = allocate(size, numpy.int32)
+    vertices = allocate(2*vertices, numpy.float32)
+    d_bitmap = numpy.intp(bitmap.base.get_device_pointer())
+    d_points = numpy.intp(points.base.get_device_pointer())
+
+    #generate/read input data
+    numpy.copyto(points, numpy.random.randn(2*size).astype(numpy.float32))
+    numpy.copyto(vertices, numpy.fromfile("vertices.dat", dtype=numpy.float32))
+
+    #allocate gpu device memory for storing the vertices
+    d_vertices = drv.mem_alloc(vertices.nbytes)
+
+    #copy from host memory to GPU device memory
+    drv.memcpy_htod(d_vertices, vertices)
+
+    #kernel arguments
+    gpu_args = [d_bitmap, d_points, d_vertices, size]
+
+    #setup thread block sizes
+    threads = (256, 1, 1)
+    grid = (int(numpy.ceil(size/float(threads[0]))), 1)
+
+    #create events for time measurement
+    start = drv.Event()
+    end = drv.Event()
+
+    #warm up the device a bit before measurement
+    context.synchronize()
+    for i in range(5):
+        pnpoly_kernel(*gpu_args, block=threads, grid=grid)
+    context.synchronize()
+
+    #run the kernel and measure time using events
+    start.record()
+    pnpoly_kernel(*gpu_args, block=threads, grid=grid)
+    end.record()
+    context.synchronize()
+    print("cn_pnpoly took", end.time_since(start), "ms.")
+
+    #compute the reference answer using the reference kernel
+    reference = allocate(size, numpy.int32)
+    d_reference = numpy.intp(reference.base.get_device_pointer())
+    reference_kernel = module.get_function("cn_pnpoly_reference_kernel")
+    ref_args = [d_reference, d_points, d_vertices, size]
+    context.synchronize()
+    start.record()
+    reference_kernel(*ref_args, block=threads, grid=grid)
+    end.record()
+    context.synchronize()
+    print("reference kernel took", end.time_since(start), "ms.")
+
+
+    #check if the result is the same
+    test = numpy.sum(numpy.absolute(bitmap - reference)) == 0
+    if test != True:
+        print("answer:")
+        print(bitmap)
+        print("reference:")
+        print(reference)
+    else:
+        print("ok!")
+
+
+
+if __name__ == "__main__":
+    drv.init()
+    context = drv.Device(0).make_context()
+    try:
+        #get compute capability for compiling CUDA kernels
+        devprops = { str(k): v for (k, v) in context.get_device().get_attributes().items() }
+        cc = str(devprops['COMPUTE_CAPABILITY_MAJOR']) + str(devprops['COMPUTE_CAPABILITY_MINOR'])
+
+        run_pnpoly(context, cc)
+    finally:
+        context.pop()
