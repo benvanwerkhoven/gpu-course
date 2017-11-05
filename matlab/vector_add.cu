c7e548a6b41d80207f7d36d62a3d8ce736b09611
Ben van Werkhoven <b.vanwerkhoven@esciencecenter.nl>
2017-11-05 21:04:16 +0100

    finalizing the Matlab exercises

--- matlab/vector_add.cu
+++ matlab/vector_add.cu
@@ -1,5 +1,5 @@
 __global__ void vector_add(float *c, const float *a, const float *b, const int n) {
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int i = threadIdx.x;  /* <--- Oops! something is not right here! */ 
     if (i<n) {
         c[i] = a[i] + b[i];
     }
